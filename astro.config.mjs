892994cc6898298cf28e6902abfec698db9d6b2d
MuhammadHafijur <85216180+MuhammadHafijur@users.noreply.github.com>
2024-10-12 21:38:40 +0600

    :recylcle: update: Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
 
+import vercel from '@astrojs/vercel/static';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  output: 'server',
+  output: "static",
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
